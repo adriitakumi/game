7293ab3d2d009952cfefa412cabe927ccae23744
adriitakumi <nicolettekagamine@gmail.com>
2017-02-07 21:32:17 +0800

    synopsis and character prof

--- application/views/index.php
+++ application/views/index.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
 	<title>Welcome to CodeIgniter</title>
 	<link rel="stylesheet" type="text/css" href="css/customize.css" />
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
@@ -15,12 +16,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">Reality Realm</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Developers</a></li>
+      <li><a href="#">Contact Us</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
@@ -34,10 +35,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="glitch" data-text="REALITY REALM">REALITY REALM</div>
 </div>
 </div> 
-<div id = "summary"><a href= "<?php echo site_url('welcome/qwer') ?>">LAAAAAAAAAAAAAAAAAAAAAAAAAH</a>
-Scroll Up and Down this page to see the parallax scrolling effect.
-This div is just here to enable scrolling. Scroll Up and Down this page to see the parallax scrolling effect.
-This div is just here to enable scrolling. Scroll Up and Down this page to see the parallax scrolling effect. This div is just here to enable scrolling.
+<div class = "synopsis">
+<h1><div class="glitchh" data-text="Synopsis">Synopsis<br><br></div></h1>
+<div class = "text">
+  <p>
+  ## Hello? Can anyone read this?<br>
+  ## I don't know if you can see this, but I need your help.<br>
+  ## I need you to s̝̲̪̬̀t̴͚͍͙̼̘̟ͅo͚̣̙͝ͅṕ̥̳̳̫ ҉̭h̴̰̜̣̳͍e͕̣͖̮̫̯͘ṛ̹̳ͅ ̖̰͕̩̰̪̜b̞̪̺̬e̺̥̻͔̩f̘͇̩̖̳͞o̱̜̠̗̬̼͉r̖̦̭̩͉̳͙e̛̹̫ ̴͍̳̟i̙̺͎͕ͅt̙̠̟͝'̞̘̕s͏̼̞ ̹̦̱͕̦̰t̻̞o̭o̩̭̘̘̙̟ ͏̖̮̱̖l͈̫̭̯a̮̖̤̘̩̩͘t̳̱͇̹e͙͟.̬̞̩̹̗͚͡ <br><br><br><br>
+
+  Milo, Nido anf Energen has just moved into an old mansion with their mom, Anmum. After a couple hurriedly leaves the mansion, a spooky backstory about the house is revealed, and some seemingly unexplainable events occur, Milo comes to a conclusion...<br><br>
+
+  He realises that he is in a horror game. A <i>badly-written</i> horror game.<br><br>
+
+  And he wants to leave... <br><br>
+  <hr>
+  <br>
+  </p>
+<h1><div class="glitchh" data-text="Characters">Characters<br><br></div></h1>
+<div class="table-responsive"><center>
+<table>
+  <tr>
+  <td><img src = "img/milo.png"/></td>
+  <td>Milo<br><br>Kinda main protagonist?? Siya mostly yung magsasalita eh pero ewan ko lang hahahahahahaha i wanna die kill me pls.</td>
+  </tr>
+  <tr>
+    <td>Energen<br><br>Childhood friend ni Milo at Nido. Si forever girl WALANG MAG BABALENTAYNS</td>
+    <td class = "imgee"><img src = "img/energen.png" class = "imge"/></td>
+  </tr>
+  <tr>
+    <td><img src = "img/nido.png"/></td>
+    <td>Nido<br><br>Kakambal ni Milo kase wala akong makitang ibang sprite kaya kumuha nalang ako ng isa pang sprite ni Milo pero ibang facial expression hahahahahah crie pakamatay ka na Adrielle</td>
+  </tr>
+</table>
+</center>
+<br><br>
+<hr>
+</div>
+</div>
 </div>
 </body>
 </html>
